20be24b650e8127590eb39b81c2bf567f756e8b4
Dhruv Nair <33425787+DhruvNair@users.noreply.github.com>
2018-11-18 17:13:16 +0530

    Poll Bug FIx#1

--- polls/views.py
+++ polls/views.py
@@ -10,16 +10,18 @@ def index(request):
     context = {'latest_question_list': latest_question_list}
     return render(request,'polls/index.html',context)
 # Create your views here.
+@login_required
 def detail(request,question_id):
     if Voter.objects.filter(question_id=question_id,user=request.user.username).exists():
         return render(request,'polls/alreadyvoted.html')
     else:
         question = get_object_or_404(Question,pk=question_id)
         return render(request,'polls/detail.html',{'question':question})
-
+@login_required
 def results(request,question_id):
     question = get_object_or_404(Question,pk=question_id)
     return render(request,'polls/results.html',{'question':question})
+@login_required
 def vote(request,question_id):
     question = get_object_or_404(Question,pk=question_id)
     if Voter.objects.filter(question_id=question_id,user=request.user.username).exists():
@@ -37,7 +39,7 @@ def vote(request,question_id):
         selected_choice.save()
         Voter.objects.create(question_id=question_id,user=request.user.username)
         return HttpResponseRedirect(reverse('polls:results',args=[question.id]))
-
+@login_required
 def questions(request):
     return render(request,'polls/questions.html')
 def choices(request):
@@ -46,12 +48,14 @@ def choices(request):
     questionlogid = Question.objects.create(question_text=question)
     context = {'question': questionlogid.pk , 'number':list(range(int(number)))}
     return render(request,'polls/choices.html',context)
+@login_required
 def savechoices(request):
     number = request.POST['number']
     question = request.POST['question']
     questions = Question.objects.get(pk=question)
     number = [int(i) for i in number[1:-1].split(",")]
-    for x in number:
-        s = request.POST['choice'+str(x+1)]
-        choicelogid = Choice.objects.create(question = questions,choice_text=s)
+    if len(Choice.objects.filter(question=questions))==0:
+        for x in number:
+            s = request.POST['choice'+str(x+1)]
+            choicelogid = Choice.objects.create(question = questions,choice_text=s)
     return render(request,'polls/added.html',{"questionid":questions.pk})
