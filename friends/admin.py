2d3f00d6c615ec89fb359926ecc98e0d01f12416
Dhruv Nair <nair.dhruv@gmail.com>
2018-12-12 16:14:01 +0530

    Removed Friends

--- friends/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Interest_Model,Pending_Requests,Confirmed_Friends
-# Register your models here.
-admin.site.register(Interest_Model)
-admin.site.register(Pending_Requests)
-admin.site.register(Confirmed_Friends)
